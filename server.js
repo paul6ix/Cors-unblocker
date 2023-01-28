cd00a5645e54ba6eeaf0981ef9d6c7dba69f247a
paul-okpor <paul@bcw.group>
2023-01-28 16:14:17 +0100

    intial commit

---
+++ server.js
@@ -0,0 +1,41 @@
+
+var host = process.env.HOST || '127.0.0.1';
+var port = process.env.PORT || 5656;
+
+// Grab the blacklist from the command-line so that we can update the blacklist without deploying
+// again. CORS Anywhere is open by design, and this blacklist is not used, except for countering
+// immediate abuse (e.g. denial of service). If you want to block all origins except for some,
+// use originWhitelist instead.
+var originBlacklist = parseEnvList(process.env.CORSANYWHERE_BLACKLIST);
+var originWhitelist = parseEnvList(process.env.CORSANYWHERE_WHITELIST);
+function parseEnvList(env) {
+  if (!env) {
+    return [];
+  }
+  return env.split(',');
+}
+
+var checkRateLimit = require('./lib/rate-limit')(process.env.CORSANYWHERE_RATELIMIT);
+
+var cors_proxy = require('./lib/cors-anywhere');
+cors_proxy.createServer({
+  originBlacklist: originBlacklist,
+  originWhitelist: originWhitelist,
+  requireHeader: ['origin', 'x-requested-with'],
+  checkRateLimit: checkRateLimit,
+  removeHeaders: [
+    'cookie',
+    'cookie2',
+    'x-request-start',
+    'x-request-id',
+    'via',
+    'connect-time',
+    'total-route-time',
+  ],
+  redirectSameOrigin: true,
+  httpProxyOptions: {
+    xfwd: false,
+  },
+}).listen(port, host, function() {
+  console.log('Running ARKHIA CORS on ' + host + ':' + port);
+});
